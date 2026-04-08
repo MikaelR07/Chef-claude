fbba25f462779f2207b052c90fd006ac8d362112
Victor Michael <victormudoga98@gmail.com>
2026-04-08 21:12:52 +0300

    added particles backround successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import Header from "./components/header";
 import Main from "./components/main";
+import Particles from "./components/Particles";
 function App() {
   return (
     <>
-      <Header />
-      <Main />
+      <Particles
+        particleColors={["#ffffff"]}
+        particleCount={200}
+        particleSpread={10}
+        speed={0.1}
+        particleBaseSize={100}
+        moveParticlesOnHover
+        alphaParticles={false}
+        disableRotation={false}
+        pixelRatio={1}
+      />
+      <div className="app-content">
+        <Header />
+        <Main />
+      </div>
     </>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,14 +2,14 @@ import chefIcon from "../assets/images/chef.png";
 export default function Header() {
   return (
     <header>
-      <nav className="navigation-bar">
+      <nav className="navigation-bar ">
         <div className="logo-container">
           <img className="logo-image" src={chefIcon} alt="Chef" />
-          <h1 className="logo-name">
+          <h1 className="logo-name text-5xl font-medium">
             <span>Chef Claude</span>
           </h1>
         </div>
-        <ul className="links">
+        <ul className="links text-3xl">
           <li>Inventory</li>
           <li>Recipes</li>
           <li>Contact</li>
