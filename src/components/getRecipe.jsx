74ffd6a987703ed662b8347b412a973c2987fa1f
Victor Michael <victormudoga98@gmail.com>
2026-04-09 12:24:05 +0300

    added the getRecipe componenent

---
+++ src/components/getRecipe.jsx
@@ -0,0 +1,11 @@
+export default function GetRecipe() {
+  return (
+    <div className="recipe-request">
+      <div className="get-recipe">
+        <h1>Ready for a recipe?</h1>
+        <p>Generate a recipe from your list of ingredients.</p>
+      </div>
+      <button className="get-recipe-button">Get a recipe</button>
+    </div>
+  );
+}
